e86d113f484a0666ca40615b7dd607a6eb706a41
Kasi26 <kasiniventhan16@gmail.com>
2026-09-19 20:25:04 +0000

    Initial Jenkins CI pipeline

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Jenkins Production CI/CD Demo");
+});
+
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "UP"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Application running on port ${PORT}`);
+});
